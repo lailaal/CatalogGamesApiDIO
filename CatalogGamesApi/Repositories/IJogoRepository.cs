2c154260e7927d87a49f6235829a744a08ab6cec
unknown <lailaaraujolima@gmail.com>
2021-09-29 14:51:58 -0300

    Iniciando repositorio CatalogGames

---
+++ CatalogGamesApi/Repositories/IJogoRepository.cs
@@ -0,0 +1,17 @@
+﻿using CatalogGames.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CatalogGamesApi.Repositories
+{
+    public interface IJogoRepository : IDisposable
+    {
+        Task<List<CatalogGames.Entities.Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
+    }
+}
---
+++ CatalogGamesApi/Services/IJogoService.cs
@@ -0,0 +1,18 @@
+﻿using CatalogGamesApi.InputModel;
+using CatalogGamesApi.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CatalogGames.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
